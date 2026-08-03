3585f920ea3fdd3e0878607bd7b4af0fbec6c4a2
sndsena2023 <snd.sena2023@gmail.com>
2026-08-03 22:34:11 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,4 +1,3 @@
-// กำหนดค่า Firebase ของคุณ
 const firebaseConfig = {
     apiKey: "AIzaSyAkFXyHA4AyvZTZQd0CquCl-zRdUpUo66I",
     authDomain: "student-care-app-ccf58.firebaseapp.com",
@@ -8,11 +7,8 @@ const firebaseConfig = {
     appId: "1:104140224696:web:f31be03111fbc889c5dc4d"
 };
 
-// เริ่มต้นระบบ Firebase ทันทีที่โหลดไฟล์นี้
 if (!firebase.apps.length) {
     firebase.initializeApp(firebaseConfig);
 }
-
-// สร้างตัวแปร db ไว้เรียกใช้ในไฟล์อื่นๆ
 const db = firebase.firestore();
 console.log("🔥 เชื่อมต่อฐานข้อมูล Firebase สำเร็จ!");
